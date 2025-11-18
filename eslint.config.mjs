3548896df52fd50c0827a2e59c859be8fabb0c67
Dapuluous <mdhaffamahendra@gmail.com>
2025-11-18 14:57:19 +0700

    feat: nuxt4 directory

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,5 @@
 import perfectionist from "eslint-plugin-perfectionist";
-import withNuxt from "./.nuxt/eslint.config.mjs";
+import withNuxt from ".nuxt/eslint.config.mjs";
 
 export default withNuxt(
   {
